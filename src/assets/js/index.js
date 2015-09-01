db89a23965511f0651e9ad91a89f16f01104ba6a
lockys <locky4567@gmail.com>
2015-09-01 23:13:06 +0800

    first commit

---
+++ src/assets/js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  var romanize = require('romanize-names');
+  var valToSystem = {
+    'wg (default)': 'WG',
+    'mps-ii': 'MPS-II',
+    'hanyu': 'HANYU'
+  };
+  var system = 'WG';
+
+  $('.dropdown').dropdown({
+    onChange: function(val) {
+      console.log((valToSystem[val]));
+      system = valToSystem[val];
+      $('#output').html(romanize($('#input').val(), system));
+    },
+  });
+
+  $('#input').bind('input propertychange', function() {
+    $('#output').html(romanize(this.value, system));
+  });
+});
